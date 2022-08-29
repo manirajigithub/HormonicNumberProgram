1b4b8e5c2360aeba7868616a7ea5b9d657b0d84c
mani1 <mani1@NANI143>
2022-08-29 11:01:16 +0530

    [Rajeswari]Added Hormonic Number

---
+++ HormonicNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HornomicNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC1_HornomicNumber.CheckUC1_HornomicNumber();
+        }
+    }
+}
---
+++ HormonicNumber/UC1_HormonicNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace HornomicNumber
+{
+    public static class UC1_HornomicNumber
+    {
+        public static void CheckUC1_HornomicNumber()
+        {
+
+
+            int i, n;
+            n = 50;
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write("1/{0} + ", i);
+
+            }
+            if (n == 50)
+            {
+                Console.WriteLine("The nth Harmonic Number : 1/{0} ", n);
+            }
+        }
+    }
+}
